4db88b61bcb3bf3e7276cf6e92c5729e3aba9c39
Jefferson Simão Gonçalves <gerson.simao.92@gmail.com>
2025-03-04 08:45:00 -0300

    refactor(config): remove unused PHPStan error ignore rules
    clean up `phpstan.neon.dist` by removing unused `ignoreErrors` section
    adjust import order in `UmamiServiceProvider` for readability

--- src/UmamiServiceProvider.php
+++ src/UmamiServiceProvider.php
@@ -2,8 +2,8 @@
 
 namespace JeffersonGoncalves\Umami;
 
-use Spatie\LaravelPackageTools\PackageServiceProvider;
 use Spatie\LaravelPackageTools\Package;
+use Spatie\LaravelPackageTools\PackageServiceProvider;
 
 class UmamiServiceProvider extends PackageServiceProvider
 {
